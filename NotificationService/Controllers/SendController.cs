face89f29ad5e059fb3c7cac04f17f7bd5be1fc0
shah@mobilityone.com.my <shah@mobilityone.com.my>
2018-09-19 15:43:29 +0800

    put comment on terminal api

--- NotificationService/Controllers/SendController.cs
+++ NotificationService/Controllers/SendController.cs
@@ -22,6 +22,7 @@ namespace NotificationService.Controllers
         [ModelStateValidation]
         public async Task<IHttpActionResult> Terminal(string TID, string MID, string Title, string Body, int NotificationType)
         {
+            //only can be access locally
             if (!Request.IsLocal())
             {
                 throw new HttpResponseException(HttpStatusCode.NotFound);
